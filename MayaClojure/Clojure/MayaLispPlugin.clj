794ae21c32f4e7978048ce654c8d73de2076af8e
iamsleepy <sos1096@hotmail.com>
2016-03-31 11:33:48 +0800

    Add comments

--- MayaClojure/Clojure/MayaLispPlugin.clj
+++ MayaClojure/Clojure/MayaLispPlugin.clj
@@ -1,3 +1,5 @@
+; It will generate a System.Runtime.Assembly.LoadFrom call, so we'd better to put it in the correct working path. 
+; openmayacs.dll is located inside of the bin folder, so I'll make it same as the Maya folder structure.
 (assembly-load-from "bin/openmayacs.dll")
 (ns MayaLispPlugin
 	(:require [clojure.core])	
@@ -5,10 +7,12 @@
 		:name MayaLispPlugin.helloWorldCmd
 		:extends Autodesk.Maya.OpenMaya.MPxCommand
 		:implements [Autodesk.Maya.OpenMaya.IMPxCommand]		
+		; We need a dll, not executable
 		:main false				
 		)
 	)
 
+; Override do it function, clojureClr uses invoke functions, so the first parameter should be instance going to be invoked. Then, we'll have usual MArgList parameter.
 (defn -doIt [this args] 
 	(Autodesk.Maya.OpenMaya.MGlobal/displayInfo "Hello World!\n"))
 
--- MayaClojure/MayaLispPlugin.cs
+++ MayaClojure/MayaLispPlugin.cs
@@ -2,4 +2,5 @@
 using Autodesk.Maya.OpenMaya;
 using MayaLispPlugin;
 
+// It seems that assembly is not supported by ClojureClr so far. So I need a .net wrapper here
 [assembly: MPxCommandClass(typeof(MayaLispPlugin.helloWorldCmd), "helloWorldCmd")]
